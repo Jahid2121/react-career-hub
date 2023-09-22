c5f067487c9e37e1cb3a04f9546298f8ef652175
Jahid2121 <jahidsarkar2121@gmail.com>
2023-09-22 14:43:38 +0600

    Add Icons and handle Show All jobs

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,3 +1,7 @@
+import { MdLocationOn } from "react-icons/md";
+import { AiOutlineDollar } from "react-icons/ai";
+
+
 const Job = ({ job }) => {
   const { logo, job_title,company_name,remote_or_onsite,location, job_type, salary} = job;
   return (
@@ -13,6 +17,10 @@ const Job = ({ job }) => {
         <div>
             <button className="px-5 py-2 font-extrabold border border-[#7E90FE] rounded mr-4 text-[#7E90FE]">{remote_or_onsite}</button>
             <button className="px-5 py-2 font-extrabold border border-[#7E90FE] rounded text-[#7E90FE]">{job_type}</button>
+        </div>
+        <div className="mt-4 flex gap-6">
+          <h2 className="flex"><MdLocationOn className="text-2xl mr-2"></MdLocationOn>{location}</h2>
+          <h2 className="flex"><AiOutlineDollar  className="text-2xl mr-2"></AiOutlineDollar> {salary}</h2>
         </div>
         <div className="card-actions">
           <button className="btn btn-primary">View Details</button>
